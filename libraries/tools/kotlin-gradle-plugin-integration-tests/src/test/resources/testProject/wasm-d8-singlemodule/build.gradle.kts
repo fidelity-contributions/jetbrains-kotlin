oceanwaveshipping02-sketch/kotlin

[Wasm] Add single module integration test

libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/wasm-d8-multimodule/build.gradle.kts
@@ -1,0 +1,38 @@
+@file:OptIn(ExperimentalWasmDsl::class)
+
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+
+plugins {
+    id("org.jetbrains.kotlin.multiplatform")
+}
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+kotlin {
+    wasmJs {
+        binaries.executable()
+        d8 {
+            runTask {
+                val directory = this.inputFileProperty.get().asFile.parentFile
+                inputFileProperty.set(File(directory, "_wasm-d8-multimodule_.mjs"))
+            }
+        }
+        val multiModuleMode = if ("MULTIMODULE_MODE_STDLIB_TO_COMPILE".isEmpty()) "wasm-d8-multimodule" else "kotlin"
+        compilerOptions {
+            freeCompilerArgs.add("-Xwasm-single-module=$multiModuleMode")
+        }
+    }
+}
+
+tasks.register<Copy>("stdlibCopy") {
+    from(File(project.buildDir, "wasm"))
+    into(File(project.buildDir, "stdlib"))
+}
+
+tasks.register<Copy>("stdlibCopyBack") {
+    from(File(project.buildDir, "stdlib"))
+    into(File(project.buildDir, "wasm"))
+}

libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/wasm-d8-multimodule/src/wasmJsMain/kotlin/A.kt
@@ -1,0 +1,8 @@
+/*
+ * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+fun main() {
+    println("Multimodule is FINE!")
+}

fidelity-contributions/jetbrains-kotlin

libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/wasm-d8-singlemodule/build.gradle.kts
@@ -1,0 +1,38 @@
+@file:OptIn(ExperimentalWasmDsl::class)
+
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+
+plugins {
+    id("org.jetbrains.kotlin.multiplatform")
+}
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+kotlin {
+    wasmJs {
+        binaries.executable()
+        d8 {
+            runTask {
+                val directory = this.inputFileProperty.get().asFile.parentFile
+                inputFileProperty.set(File(directory, "_wasm-d8-singlemodule_.mjs"))
+            }
+        }
+        val moduleToCompile = if ("NOW_COMPILE_STDLIB_AS_SINGLE_MODULE".isEmpty()) "wasm-d8-singlemodule" else "kotlin"
+        compilerOptions {
+            freeCompilerArgs.add("-Xwasm-single-module=$moduleToCompile")
+        }
+    }
+}
+
+tasks.register<Copy>("stdlibCopy") {
+    from(File(project.buildDir, "wasm"))
+    into(File(project.buildDir, "stdlib"))
+}
+
+tasks.register<Copy>("stdlibCopyBack") {
+    from(File(project.buildDir, "stdlib"))
+    into(File(project.buildDir, "wasm"))
+}

libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/wasm-d8-singlemodule/src/wasmJsMain/kotlin/A.kt
@@ -1,0 +1,8 @@
+/*
+ * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+fun main() {
+    println("SingleModule is FINE!")
+}